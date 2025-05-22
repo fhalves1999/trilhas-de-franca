e525806d39575ed18a68f64a687c2f6f62e082a7
Flavio Alves <flavio.alves@perceptvision.com.br>
2025-05-22 11:23:20 -0300

    Refatoração para consumir backend seguro no Render

--- src/app/services/strava.service.ts
+++ src/app/services/strava.service.ts
@@ -1,22 +1,17 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StravaService {
-  private baseUrl = 'https://www.strava.com/api/v3';
-  private athleteId = '102459537';
-  private accessToken = 'e56739853aa1fdecf2cafa446ef9e6451926e7c1';
+  // Use a URL do seu backend no Render
+  private backendUrl = 'https://strava-trilhas-backend.onrender.com';
 
   constructor(private http: HttpClient) {}
 
   getRoutes(): Observable<any> {
-    const headers = new HttpHeaders({
-      'Authorization': `Bearer ${this.accessToken}`
-    });
-
-    return this.http.get(`${this.baseUrl}/athletes/${this.athleteId}/routes`, { headers });
+    return this.http.get(`${this.backendUrl}/api/strava/routes`);
   }
 }
